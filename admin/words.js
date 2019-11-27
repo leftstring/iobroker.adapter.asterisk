d7af630d65208ddd6ea9cabccc89a9ca22a08d39
Sebastian W <leftstringtheory@gmail.com>
2019-11-27 13:15:44 +0100

    -added asterisk admin fields
    -added config object

--- admin/words.js
+++ admin/words.js
@@ -2,16 +2,88 @@
 "use strict";
 
 systemDictionary = {
-	"username": {
-		"en": "Username",
-		"de": "Benutzername"
-	},
-	"password": {
-		"en": "Password",
-		"de": "Passwort"
-	},
-	"ipAddress": {
-		"en": "IP Address",
-		"de": "IP-Adresse"
-	}
+    "username": {
+        "en": "Username",
+        "de": "Benutzername"
+    },
+    "password": {
+        "en": "Password",
+        "de": "Passwort"
+    },
+    "ipAddress": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    },
+    "asteriskPrivateIdentity": {
+        "en": "IP Address",
+        "de": "IP-Adresse"
+    }
 };
