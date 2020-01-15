6bf064bb6eee564a65bdaf069f8120c31ede12a1
Sebastian W <leftstringtheory@gmail.com>
2020-01-15 13:12:35 +0100

    x

--- widgets/asterbird/js/asterbird.js
+++ widgets/asterbird/js/asterbird.js
@@ -1,9 +1,9 @@
 "use strict";
 
 let sipCommunication;
-let sipCommunicationAccount
+let sipCommunicationAccount;
 
-console.log("start widget");
+console.log("ASTERBIRD ", "start widget");
 
 vis.binds.asterbird = {
 	version: "0.9.0",
@@ -12,12 +12,12 @@ vis.binds.asterbird = {
     init: function (adapterInstance) {
 		vis.binds.asterbird.adapterInstance = adapterInstance;
 
-		console.log("Passed init method");
+		console.log("ASTERBIRD ","Passed init method");
 	},
 	initSIP: function(audioElement) {
 		if(!vis.editMode) {
 			vis.conn.getStates(null, (error, data) => {
-				console.log("Start initSIP method");
+				console.log("ASTERBIRD ","Start initSIP method");
 				vis.updateStates(data);
 
 				audioElement.volume = 0.5;
@@ -34,18 +34,19 @@ vis.binds.asterbird = {
 
 					if (sipCommunicationAccount.IsCorrectInitialized()) {
 						if(!sipCommunication) {
+							console.log("ASTERBIRD ","Create new SIP comm.");
 							sipCommunication = new SIPCommunication(realm, sipCommunicationAccount, websocket_proxy_url, audioElement);
 							sipCommunication.onCallIncoming = vis.binds.asterbird.onCallIncoming;
 							sipCommunication.onCallTerminated = vis.binds.asterbird.onCallTerminated;
 						} else {
-							console.log("Sip comm already there.");
+							console.log("ASTERBIRD ","Sip comm already there.");
 						}
 					} else {
 						vis.binds.asterbird.requestAsteriskAccountData(realm, websocket_proxy_url, audioElement);
 					}
 
 
-				console.log("Passed initSIP method");
+				console.log("ASTERBIRD ","Passed initSIP method");
 			});
 		}
 	},
@@ -57,19 +58,19 @@ vis.binds.asterbird = {
 		}
 	},
 	onCallIncoming: function() {
-		console.log("call incoming");
+		console.log("ASTERBIRD ","call incoming");
         vis.binds.asterbird.isCallIncoming = true;
 		vis.binds.asterbird.widgetElement.style.visibility = "visible";
 
 		var videoElement = document.getElementById("videoElement");
 		vis.binds.asterbird.intervall = setInterval(()=> {
-			console.log("update preview image");
+			console.log("ASTERBIRD ","update preview image");
 			videoElement.src = undefined;
 			videoElement.src = vis.states[vis.binds.asterbird.adapterInstance + ".imageSource.val"];
 		}, 1000)
 	},
 	onCallTerminated: function() {
-		console.log("call terminated");
+		console.log("ASTERBIRD ","call terminated");
 
 		vis.binds.asterbird.widgetElement.style.visibility = "hidden";
 
@@ -84,11 +85,11 @@ vis.binds.asterbird = {
         vis.binds.asterbird.isInCall = false;
 	},
 	acceptCall: function() {
-		console.log("accept call");
+		console.log("ASTERBIRD ","accept call");
 		sipCommunication.acceptCall();
         vis.binds.asterbird.isInCall = true;
         vis.binds.asterbird.isCallIncoming = false;
-		console.log("show video stream");
+		console.log("ASTERBIRD ","show video stream");
 		if(vis.binds.asterbird.intervall) {
 			clearInterval(vis.binds.asterbird.intervall);
 		 	vis.binds.asterbird.intervall = undefined;
@@ -133,7 +134,7 @@ vis.binds.asterbird = {
 		audioElement.volume = volumeSlider.value;
 	},
 	requestAsteriskAccountData: function (realm, websocket_proxy_url, audioElement) {
-		console.log("Open dialog for asterisk account data.")
+		console.log("ASTERBIRD ","Open dialog for asterisk account data.")
 		const accountDataDialog = document.getElementById("accountDataDialog");
 		dialogPolyfill.registerDialog(accountDataDialog);
 
